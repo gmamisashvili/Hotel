102a2f9a624182f58fc6dfe2a9eb37d9bd1fe005
gmamisashvili <giorgimamisashvili@geolab.edu.ge>
2019-05-11 17:33:11 +0400

    finished

--- jquery.js
+++ jquery.js
@@ -10,4 +10,29 @@ $(document).ready(function() {
   if ($(window).width() <= 700) {
     $(".form-header").text("დაგვიტოვეთ თქვენი ნომერი და ჩვენ დაგირეკავთ");
   }
+
+  $(".form-tag").submit(function(e) {
+    let formName = $(".contact-inputs-name");
+    let formMobile = $(".contact-inputs-mobile");
+
+    if (formName.val() == "" && formMobile.val() == "") {
+      $(".both-error").show();
+      return false;
+    } else if (formName.val() == "" && formMobile.val() != "") {
+      $(".name-error").show();
+      $(".mobile-error").hide();
+      $(".both-error").hide();
+      return false;
+    } else if (formMobile.val() == "") {
+      $(".mobile-error").show();
+      $(".name-error").hide();
+      $(".both-error").hide();
+      return false;
+    } else {
+      $(".mobile-error").hide();
+      $(".name-error").hide();
+      $(".both-error").hide();
+      return true;
+    }
+  });
 });
